ed4320c0e4e404654b816d77e3c2cf26f7540287
Gonzalo Alexander Robles Muñoz <Garm0607@gmail.com>
2025-05-27 22:09:17 -0600

    Pal main

--- El gran juego/Source.cpp
+++ El gran juego/Source.cpp
@@ -15,9 +15,8 @@
 using namespace std;
 
 
-//Genarador de números aleatorios 
+//Genarador de números aleatorios by ChatGPT
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int generarNumeroAleatorio(int min, int max) //Generador de números aleatorios.
 {
     static std::mt19937 generador(static_cast<unsigned int>(std::time(0)));
@@ -25,221 +24,401 @@ int generarNumeroAleatorio(int min, int max) //Generador de números aleatorios.
     return distribucion(generador);
 }
 
+//Imprimir lento by ChatGPT
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//Clase Jugador
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Jugador
+void imprimirLento(const std::string& texto, int retrasoMilisegundos)
 {
-    private:
+    for (char c : texto)
+    {
+        std::cout << c << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(retrasoMilisegundos));
+    }
+}
 
-        int oroInicialJugador;
-        string nombreJugador;
-        int salud;
-        int ataque;
-        int defensa;
-        int zi;
-
-        Jugador(std::string nombreJugador, int salud, int ataque, int defensa)
-        {
-            this->salud = salud;
-        
-        }
-
+//Clase Item
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Item {
+protected:
+    string nombre;
+    string tipo;
+    int cantidad;
 
 public:
-    Jugador()
-    {
-        nombreJugador = nombreJugador;
-        oroInicialJugador = 200;
+    Item(string _nombre, string _tipo, int _cantidad)
+        : nombre(_nombre), tipo(_tipo), cantidad(_cantidad) {}
+
+    virtual void usar() = 0;
+
+    string getNombre() const { return nombre; }
+    string getTipo() const { return tipo; }
+    int getCantidad() const { return cantidad; }
+
+    void reducirCantidad(int c) { cantidad -= c; }
+
+    virtual void mostrarInfo() const {
+        cout << nombre << " | Tipo: " << tipo << " | Cantidad: " << cantidad << endl;
     }
 
-    void funcMostrarInfoJugador()
-    {
-        cout << "Nombre: " << nombreJugador << " Oro inicial: " << oroInicialJugador << endl;
-    }
+    virtual int getCuracion() const { return 0; }
 
-    int getOroJugador()
-    {
-        return oroInicialJugador;
-    }
-
-    void setRestartOroJugador(Pocion paramPocion)
-    {
-        oroInicialJugador -= paramPocion.getPrecioPocion();
-    }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//Generador de enemigos
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Enemigo
-{
-
-
-private:
-
-    string nombreEnemigo;
-    int vidaEnemigo;
-    int escudoEnemigo;
-    int ataqueEnemigo;
-    int velocidadEnemigo;
-
-    //lista para posibles nombres de enemigos
-    vector<string>NombresPosiblesEnemigo = { "Goblin", "Orco", "Troll", "Bandido", "Esqueleto", "Asesino" }; //se inicia con posicion 0
-
-public:
-
-    //constructor de la clase enemigo, tiene que llevar el mismo nombre de la variable atribuida al constructor
-    Enemigo()
-    {
-        //Seleccionar nombre aleatorio del vector 
-        nombreEnemigo = NombresPosiblesEnemigo.at(1 + rand() % 5);
-        vidaEnemigo = rand() % (150 - 50) + 50; // Generar un número random entre 50 y 150.
-        escudoEnemigo = 100;
-        ataqueEnemigo = rand() % (200 - 80) + 80;
-        velocidadEnemigo = rand() % (15 - 5) + 5;
-
-    }
+    virtual ~Item() {}
 };
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//Ssitema de peleas
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    void atacar(Jugador& nombreEnemigo)
-    {
-        int daño = std::max(0, ataque - nombreEnemigo.defensa);
-        daño += generarNumeroAleatorio(0, 29);
-        nombreEnemigo.salud -= daño;
-        std::cout << nombreJugador << " ataca a " << nombreEnemigo.nombreJugador << " causando " << daño << " de daño." << std::endl;
-        if (nombreEnemigo.salud < 0) nombreEnemigo.salud = 0;
-    }
-
-    // Para mostrar el estado
-    void mostrarEstado() const 
-    {
-        std::cout << nombreJugador << " - Salud: " << salud << std::endl;
-    }
-
-    // Para verificar si está vivo
-    bool estaVivo() const 
-    {
-        return salud > 0;
-    }
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 //Objeto Poción 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Pocion
-{
-private:
-    int cantidadPocion;
-    int precioPocion;
-    int nivelPocion;
-    string descripcionEfectoPocion;
-    string nombrePocion;
+class Pocion : public Item {
+    string descripcion;
+    int curacion;
 
 public:
-    Pocion(int _cantidadPocion, int _precioPocion, int _nivelPocion, string _descripcionEfectoPocion, string _nombrePocion)
-    {
-        cantidadPocion = _cantidadPocion;
-        precioPocion = _precioPocion;
-        nivelPocion = _nivelPocion;
-        descripcionEfectoPocion = _descripcionEfectoPocion;
-        nombrePocion = _nombrePocion;
+    Pocion(string _nombre, int _cantidad, string _descripcion, int _curacion)
+        : Item(_nombre, "pocion", _cantidad), descripcion(_descripcion), curacion(_curacion) {}
+
+    void usar() override {
+        cout << "Usaste " << nombre << ": " << descripcion << " +" << curacion << " HP." << endl;
     }
 
-    void funcMostrarInfoPocion()
-    {
-        cout << " Nombre: " << nombrePocion << ", nvl: " << nivelPocion << ", desc: " << descripcionEfectoPocion << ", cantidad: " << cantidadPocion << endl;
-        cout << "Precio: " << precioPocion << " de oro" << endl;
+    void mostrarInfo() const override {
+        Item::mostrarInfo();
+        cout << "   Efecto: " << descripcion << " (+ " << curacion << " HP)" << endl;
     }
 
-    int getPrecioPocion()
-    {
-        return precioPocion;
+    int getCuracion() const override {
+        return curacion;
     }
 };
 
+//Clase Jugador
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//Objeto Tienda
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////M//////
-
-class Tienda
-{
-private:
-    vector<Pocion> listaPociones;
+class Jugador {
+    string nombre;
+    int salud;
+    int saludMaxima;
+    int ataque;
+    int defensa;
+    int oro;
+    vector<Item*> inventario;
 
 public:
-    Tienda()
-    {
-        listaPociones.push_back(Pocion(3, 50, 2, "Restaura 50 puntos de vida", "Pocion de vida"));
-        listaPociones.push_back(Pocion(8, 100, 5, "Restaura 40 puntos de mana", "Pocion de mana"));
-        listaPociones.push_back(Pocion(2, 150, 10, "Restaura 100 puntos de vida", "Elixir mayor de vida"));
-    }
+    Jugador(string _nombre)
+        : nombre(_nombre), salud(200), saludMaxima(300),
+        ataque(generarNumeroAleatorio(20, 50)), defensa(25), oro(100) {}
 
-    void funcMostrarMenu()
-    {
-        cout << "---- Bienvenido a la tienda de pociones ----" << endl;
-        cout << "Selecciona un numero del menu:" << endl;
-
-        for (size_t i = 0; i < listaPociones.size(); i++)
-        {
-            cout << i + 1 << ".- ";
-            listaPociones[i].funcMostrarInfoPocion();
+    void agregarItem(Item* item) {
+        for (auto& i : inventario) {
+            if (i->getNombre() == item->getNombre()) {
+                i->reducirCantidad(-item->getCantidad());
+                delete item;
+                return;
+            }
         }
-        cout << listaPociones.size() + 1 << ".- Salir" << endl;
+        inventario.push_back(item);
     }
 
-    void funcComprarPocion(Jugador& paramJugador)
-    {
-        int opcionSeleccionada;
-        do
-        {
-            funcMostrarMenu();
-            cout << "Tu oro actual: " << paramJugador.getOroJugador() << endl;
-            cout << "Opcion: ";
-            cin >> opcionSeleccionada;
+    void mostrarInventario() const {
+        cout << "\n--- Inventario de " << nombre << " ---\n";
+        if (inventario.empty()) {
+            cout << "Inventario vacío.\n";
+            return;
+        }
+        for (size_t i = 0; i < inventario.size(); ++i) {
+            cout << i + 1 << ". ";
+            inventario[i]->mostrarInfo();
+        }
+    }
 
-            if (opcionSeleccionada < 1 || opcionSeleccionada > listaPociones.size() + 1)
-            {
-                cout << "Opcion invalida. Intenta de nuevo." << endl;
-                continue;
+    void usarItem() {
+        mostrarInventario();
+        if (inventario.empty()) return;
+
+        int eleccion;
+        cout << "Selecciona el número del item a usar: ";
+        cin >> eleccion;
+        eleccion--;
+
+        if (eleccion >= 0 && eleccion < inventario.size()) {
+            inventario[eleccion]->usar();
+            salud += inventario[eleccion]->getCuracion();
+            if (salud > saludMaxima) salud = saludMaxima;
+
+            inventario[eleccion]->reducirCantidad(1);
+
+            if (inventario[eleccion]->getCantidad() <= 0) {
+                delete inventario[eleccion];
+                inventario.erase(inventario.begin() + eleccion);
+                cout << "Item eliminado del inventario.\n";
             }
+        }
 
-            if (opcionSeleccionada == listaPociones.size() + 1)
-            {
-                cout << "Gracias por visitar la tienda." << endl;
-                break;
-            }
 
-            Pocion pocionSeleccionada = listaPociones[opcionSeleccionada - 1];
+        else {
+            cout << "Opción inválida.\n";
+        }
+    }
 
-            if (paramJugador.getOroJugador() >= pocionSeleccionada.getPrecioPocion())
-            {
-                paramJugador.setRestartOroJugador(pocionSeleccionada);
-                cout << "Has comprado la pocion exitosamente!" << endl;
-            }
-            else
-            {
-                cout << "No tienes suficiente oro para comprar esta pocion." << endl;
-            }
+    void mostrarEstado() const {
+        cout << nombre << " - Salud: " << salud << "/" << saludMaxima
+            << ", Ataque: " << ataque << ", Defensa: " << defensa
+            << ", Oro: " << oro << endl;
+    }
 
-        } while (true);
+    bool estaVivo() const {
+        return salud > 0;
+    }
+
+    void recibirDanio(int danio) {
+        int dañoReal = max(0, danio - defensa);
+        salud -= dañoReal;
+        if (salud < 0) salud = 0;
+        cout << nombre << " recibió " << dañoReal << " de daño. Salud restante: " << salud << endl;
+    }
+
+    int getAtaque() const { return ataque; }
+    void ganarOro(int cantidad) { oro += cantidad; }
+    bool gastarOro(int cantidad) {
+        if (oro >= cantidad) {
+            oro -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    int getOro() const { return oro; }
+
+    ~Jugador() {
+        for (auto& i : inventario) delete i;
     }
 };
 
+//Clase Enemigo
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Enemigo {
+    string nombre;
+    int salud;
+    int ataque;
+    int defensa;
 
+public:
+    Enemigo()
+    {
+        vector<string> nombres = { "Goblin", "Orco", "Troll", "Bandido", "Esqueleto", "Asesino" };
+        nombre = nombres[generarNumeroAleatorio(0, nombres.size() - 1)];
+        salud = generarNumeroAleatorio(80, 360);
+        ataque = generarNumeroAleatorio(20, 65);
+        defensa = generarNumeroAleatorio(10, 30);
+    }
+
+    void mostrarEstado() const
+    {
+        cout << nombre << " - Salud: " << salud << ", Ataque: " << ataque << ", Defensa: " << defensa << endl;
+
+        system("pause");
+    }
+
+    bool estaVivo() const {
+        return salud > 0;
+    }
+
+    void recibirDanio(int danio)
+    {
+        int dañoReal = max(0, danio - defensa);
+        salud -= dañoReal;
+        if (salud < 0) salud = 0;
+        cout << nombre << " recibió " << dañoReal << " de daño. Salud restante: " << salud << endl;
+
+        system("pause");
+    }
+
+    int getAtaque() const { return ataque; }
+    string getNombre() const { return nombre; }
+};
+
+//Clase Tienda
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Tienda {
+    struct Producto {
+        string nombre;
+        string descripcion;
+        int curacion;
+        int precio;
+    };
+
+    vector<Producto> stock = {
+        { "Poción de Vida", "Restaura 50 HP", 50, 30 },
+        { "Elixir Menor", "Restaura 30 HP", 30, 20 },
+        { "Poción Mayor", "Restaura 100 HP", 100, 50 }
+    };
+
+public:
+    void mostrarProductos() {
+        cout << "\n--- Tienda de Pociones ---\n";
+        for (size_t i = 0; i < stock.size(); ++i) {
+            cout << i + 1 << ". " << stock[i].nombre << " - " << stock[i].descripcion
+                << " | Cura: " << stock[i].curacion << " | Precio: " << stock[i].precio << " oro\n";
+        }
+        cout << stock.size() + 1 << ". Salir\n";
+    }
+
+    void comprar(Jugador& jugador) {
+        int opcion;
+        while (true) {
+            mostrarProductos();
+            cout << "Oro disponible: " << jugador.getOro() << endl;
+            cout << "Selecciona el número del producto que deseas comprar: ";
+            cin >> opcion;
+
+            if (opcion == static_cast<int>(stock.size() + 1)) break;
+
+            if (opcion >= 1 && opcion <= static_cast<int>(stock.size())) {
+                const Producto& p = stock[opcion - 1];
+                if (jugador.gastarOro(p.precio)) {
+                    jugador.agregarItem(new Pocion(p.nombre, 1, p.descripcion, p.curacion));
+                    cout << "Has comprado " << p.nombre << "!\n";
+                }
+                else {
+                    cout << "No tienes suficiente oro.\n";
+                }
+            }
+            else {
+                cout << "Opción inválida.\n";
+            }
+        }
+    }
+};
+
+//Sitema de peleas
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void combate(Jugador& jugador, Enemigo& enemigo)
+{
+    system("cls");
+
+    cout << "\n--- ¡Comienza el combate! ---\n";
+    jugador.mostrarEstado();
+    enemigo.mostrarEstado();
+
+    while (jugador.estaVivo() && enemigo.estaVivo())
+    {
+        int opcion;
+
+        system("cls");
+
+        cout << "\nElige acción:\n1. Atacar\n2. Usar objeto\nOpción: ";
+        cin >> opcion;
+
+        if (opcion == 1)
+        {
+            enemigo.recibirDanio(jugador.getAtaque());
+        }
+        else if (opcion == 2)
+        {
+            jugador.usarItem();
+        }
+        else
+        {
+            cout << "Opción inválida.\n";
+
+            continue;
+        }
+
+        if (enemigo.estaVivo())
+        {
+            jugador.recibirDanio(enemigo.getAtaque());
+        }
+    }
+
+    if (jugador.estaVivo())
+    {
+        int oroGanado = generarNumeroAleatorio(5, 15);
+        jugador.ganarOro(oroGanado);
+        cout << "\n¡Has derrotado al " << enemigo.getNombre() << "! Ganaste " << oroGanado << " de oro.\n";
+    }
+    else
+    {
+        cout << "\nHas sido derrotado...\n";
+    }
+}
+
+//Ya ahora si el jueguito
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    int option;
+    string textoVacio;
+    string Lento;
+
+    while (true) {
+        system("cls");
+
+        string despertar = "Despiertas en un lugar extraño.\n";
+        imprimirLento(despertar, 40);
+
+        string primeraOpcion = "Decides 1- Levantarte   2- Seguir durmiendo.\n";
+        imprimirLento(primeraOpcion, 30);
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+
+            Lento = "Muy bien con optimismo, te esperan buenas cosas en este viaje, recuerda que al que madruga Dios le ayuda.\n";
+            imprimirLento(Lento, 40);
+            break;
+
+        case 2:
+            Lento = "Pues no papito aqui no se hace lo que quieras mientras vivas en esta casa me haras caso MI CODIGO!!! MIS REGLAS!!!\n";
+            imprimirLento(Lento, 40);
+
+            break;
+
+        default:
+            cout << "opcion en silla de ruedas (invalida), intentalo de nuevo\n";
+
+            system("pause");
+
+            continue;
+        }
+
+        system("pause");
+        break;
+
+    }
+
+    system("cls");
+
+    Lento = "Bueno, ya estamos levantaditos asi que \n";
+    imprimirLento(Lento, 40);
+
+    Lento = "...\n";
+    imprimirLento(Lento, 1000);
+
+    Lento = "RWAAAAAAAAAAAAAAAAR\n";
+    imprimirLento(Lento, 20);
+
+    system("pause");
+    system("cls");
+
+    Lento = "flush\n";
+    imprimirLento(Lento, 20);
+    Lento = "flush\n";
+    imprimirLento(Lento, 20);
+    Lento = "flush\n";
+    imprimirLento(Lento, 20);
+
+    system("pause");
+    system("cls");
+
+    Lento = "WOW, viste ese dragon, nunca lo habia visto antes espero que no sea un problema\n";
+    imprimirLento(Lento, 40);
+
+    Lento = " Que dices? QUE QUE QUIERES HACER!?\n";
+    imprimirLento(Lento, 40);
+
+    Lento = "1- Ir a matarlo 2- Ir a matarlo 3- Ir a matarlo 4- Ir a matarlo n- Ir a matarlo";
+    imprimirLento(Lento, 30);
+    getline(cin, textoVacio);
+
+
+
+
+    return 0;
+}
